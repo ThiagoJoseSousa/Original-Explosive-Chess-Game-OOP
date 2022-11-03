b7b8b6dd5cba9e217dc9b36224f11b6aaf1e9f22
ThiagoJoseSousa <imigrationstudy@gmail.com>
2022-11-03 20:02:51 -0300

    click changes page

--- src/page/page.js
+++ src/page/page.js
@@ -1,3 +1,5 @@
+// IIFE for hamburguer layout
+(function (){
 const hamburguer = document.querySelector(".hamburguer");
 const navList= document.querySelector(".nav-list")
 
@@ -5,3 +7,22 @@ hamburguer.addEventListener("click", ()=>{
     hamburguer.classList.toggle("active");
     navList.classList.toggle("active");
 })
+}());
+
+//IIFE for displaying different pages when clicking on navbar/homepage buttons.
+(function (){
+    const navItems= document.getElementsByClassName('nav-item');
+    const sections=document.querySelectorAll('section');
+    //loop for adding listeners
+    for (let i=0; i<navItems.length; i++) {
+        navItems[i].addEventListener('click', displayClicked)
+    }
+    //this will add notcurrent to all classes and remove the one clicked.
+    function displayClicked(e) {
+        for (let i=0; i<sections.length; i++) {
+            sections[i].classList.add('notcurrent')
+        }
+        sections[e.target.dataset.page].classList.remove('notcurrent')
+    }
+}());
+
